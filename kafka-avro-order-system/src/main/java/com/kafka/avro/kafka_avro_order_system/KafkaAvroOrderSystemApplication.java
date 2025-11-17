67166c93811a1f2e698df1efa9759ada96084665
Yasodha <139672828+MYVirajani@users.noreply.github.com>
2025-11-17 22:18:23 +0530

    init

---
+++ kafka-avro-order-system/src/main/java/com/kafka/avro/kafka_avro_order_system/KafkaAvroOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.avro.kafka_avro_order_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAvroOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAvroOrderSystemApplication.class, args);
+	}
+
+}
---
+++ kafka-avro-order-system/src/test/java/com/kafka/avro/kafka_avro_order_system/KafkaAvroOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.avro.kafka_avro_order_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAvroOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
